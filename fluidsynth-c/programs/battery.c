30a887f59079dc8aea0d2a0e85a3615eaea13d2f
Jonathan Slenders <jonathan at slenders dot be>
2011-04-29 22:04:51 +0200

    cleaned up code.

---
+++ fluidsynth-c/programs/battery.c
@@ -0,0 +1,38 @@
+#include <time.h> /* for clock() */
+#include <pthread.h> /* posix threading */
+#include "../main.h"
+
+
+pthread_t battery_test_thread;
+
+void battery_test_start(void* data);
+
+
+/* *** Battery test ***/
+void battery_test_activate(void)
+{
+	pthread_create(&battery_test_thread, NULL, (void *) &battery_test_start, (void *) NULL);
+}
+
+void battery_test_start(void* data)
+{
+	// Reset voltage 
+	int i;
+	for (i = 0; i < BALL_COUNT; i ++)
+	{
+		juggle_states[i].voltage = 0;
+
+		// Query voltage again
+    	send_packet("BATTTEST", i, NULL, NULL);
+
+		// Wait 1sec beforing pinging the next. (avoid wireless network
+		// collisions during all the answers.)
+		sleep(1);
+	}
+}
+
+void battery_test_packet_received(struct juggle_packet_t* packet)
+{
+	print_string("ball %i: %s %s", packet->ball, packet->param1, packet->param2);
+}
+
---
+++ fluidsynth-c/programs/battery.h
@@ -0,0 +1,3 @@
+
+void battery_test_activate(void);
+void battery_test_packet_received(struct juggle_packet_t* packet);
